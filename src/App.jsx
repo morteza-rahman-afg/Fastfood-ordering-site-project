a1dea63f091f9bca43409ff3dc89a5daa090e255
Morteza Rahamn <mortezarahman.2005afg@gmail.com>
2025-11-16 14:52:35 +0330

    I added product data to the json file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,13 @@
 function App() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>title</h1>
+      <p>
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem,
+        tenetur.
+      </p>
+    </div>
+  );
 }
 
 export default App;
