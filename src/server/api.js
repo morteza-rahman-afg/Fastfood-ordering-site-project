82aaac90bd7e2bfa62e308b8988e6bf99c53f9f0
Morteza Rahamn <mortezarahman.2005afg@gmail.com>
2025-11-19 21:28:43 +0330

    Add fetch functions for product categories in API module

--- src/server/api.js
+++ src/server/api.js
@@ -2,7 +2,7 @@ export async function fetchProduct() {
   try {
     const res = await fetch("http://localhost:6001/products");
     if (!res.ok) throw new Error("not fiane fetch");
-    const data = res.json();
+    const data = await res.json();
 
     return data;
   } catch (err) {
@@ -11,9 +11,26 @@ export async function fetchProduct() {
 }
 export async function fetchProductOne(id) {
   try {
-    const res = await fetch(`http://localhost:6001/products/${id}`);
+    const res = await fetch(`http://localhost:6001/products/?id=${id}`);
     if (!res.ok) throw new Error("not fiane fetch");
     const data = res.json();
+
+    return data;
+  } catch (err) {
+    console.log(err.massage);
+  }
+}
+
+export async function fetchProductCategory(typeProduct) {
+  try {
+    const res = await fetch(
+      `http://localhost:6001/products${
+        typeProduct == "all" ? "" : "?Category=" + typeProduct
+      }`
+    );
+    if (!res.ok) throw new Error("not fiane fetch");
+
+    const data = await res.json();
 
     return data;
   } catch (err) {
