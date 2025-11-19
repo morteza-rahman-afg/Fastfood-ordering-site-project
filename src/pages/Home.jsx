f3cb2f38aa6ad23d19ea3206e9a4360c4bc26b67
Morteza Rahamn <mortezarahman.2005afg@gmail.com>
2025-11-19 21:35:27 +0330

    Add conditional rendering for Header and ProductsContainer in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,12 +1,20 @@
+import { useState } from "react";
 import Header from "../Components/Header";
 import ProductsContainer from "../Components/ProductsContainer";
-const Category = ["Main course", "Drink", "sauce"];
+const Category = ["MainCourse", "Drink", "sauce"];
 function Home() {
+  const [showData, setShowData] = useState(false);
+
   return (
     <div className="w-full">
-      <Header />
+      <Header show={showData} />
       {Category.map((title, i) => (
-        <ProductsContainer title={title} key={i} />
+        <ProductsContainer
+          title={title}
+          key={i}
+          show={showData}
+          setShow={setShowData}
+        />
       ))}
     </div>
   );
