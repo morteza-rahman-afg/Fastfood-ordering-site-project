4faff095d0adb1f6873e64213a666441fa3fb6b8
Morteza Rahamn <mortezarahman.2005afg@gmail.com>
2025-11-19 21:15:01 +0330

    Add scroll-based animation to Header using react-intersection-observer

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,21 +1,33 @@
+import { useInView } from "react-intersection-observer";
 import { Link } from "react-router-dom";
-function Header() {
+function Header({ show }) {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.5,
+  });
   return (
     <div className="w-full h-[350px] sm:h-[450px] md:h-[550px] lg:h-[650px] lg:bg-[url('h-2.png')] bg-cover flex lg:flex-col items-center lg:items-start  justify-between lg:justify-center px-5 lg:px-0">
-      <div className=" flex flex-col items-start lg:mr-16 gap-3 sm:gap-4 md:gap-y-5 lg:gap-y-6">
-        <h1 className=" font-DanaBold text-base sm:text-xl md:text-3xl lg:text-5xl text-textColor lg:text-white">
-          به فودچی خوش امدید
-        </h1>
-        <p className="text-xs sm:text-sm md:text-base lg:text-xl  text-textColor lg:text-white font-DanaMedium">
-          طعم‌های تازه و داغ، فقط چند کلیک فاصله دارن
-        </p>
-        <Link
-          className=" bg-bg1 text-white lg:text-textColor lg:bg-white rounded-md text-sm md:text-base w-24 h-9 sm:w-28 sm:h-10 md:w-40 md:h-11 text-center leading-[36px] sm:leading-[40px] md:leading-[44px] font-DanaMedium hover:border-textColor hover:text-textColor hover:bg-inherit  lg:hover:text-white border lg:hover:border-white   transition-all duration-300"
-          to={"products"}
+      {show && (
+        <div
+          ref={ref}
+          className={`${
+            inView ? "opacity-100 translate-x-0" : "opacity-0 translate-x-52"
+          } flex flex-col items-start lg:mr-16 gap-3 sm:gap-4 md:gap-y-5 lg:gap-y-6 transition-all duration-[1200ms] `}
         >
-          محصولات
-        </Link>
-      </div>
+          <h1 className=" font-DanaBold text-base sm:text-xl md:text-3xl lg:text-5xl text-textColor lg:text-white">
+            به فودچی خوش امدید
+          </h1>
+          <p className="text-xs sm:text-sm md:text-base lg:text-xl  text-textColor lg:text-white font-DanaMedium">
+            طعم‌های تازه و داغ، فقط چند کلیک فاصله دارن
+          </p>
+          <Link
+            className=" bg-bg1 text-white lg:text-textColor lg:bg-white rounded-md text-sm md:text-base w-24 h-9 sm:w-28 sm:h-10 md:w-40 md:h-11 text-center leading-[36px] sm:leading-[40px] md:leading-[44px] font-DanaMedium hover:border-textColor hover:text-textColor hover:bg-inherit  lg:hover:text-white border lg:hover:border-white   transition-all duration-300"
+            to={"products/all"}
+          >
+            محصولات
+          </Link>
+        </div>
+      )}
       <div className=" flex lg:hidden">
         <img
           src="/bg-mobile.png"
